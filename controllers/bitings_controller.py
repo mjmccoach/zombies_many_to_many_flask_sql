72280b56248d24887a0d980f9ca6ffafbf1dca6d
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-10-01 13:07:21 +0100

    biting repository added, with ability to save and select the entire list, including html rendering of biting list

--- controllers/bitings_controller.py
+++ controllers/bitings_controller.py
@@ -1,8 +1,18 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 
+from models.biting import Biting
+
+import repositories.biting_repository as biting_repository
+import repositories.human_repository as human_repository
+import repositories.zombie_repository as zombie_repository
+
 bitings_blueprint = Blueprint("bitings", __name__)
 
 # INDEX
+@bitings_blueprint.route("/bitings")
+def bitings():
+    bitings = biting_repository.select_all()
+    return render_template("/bitings/index.html", bitings = bitings)
 
 # NEW
 
